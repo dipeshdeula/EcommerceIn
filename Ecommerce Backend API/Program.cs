c63125eafa2ab343897d4c51340f1dc856619d4d
deuladipesh94@gmail.com <deuladipesh94@gmail.com>
2025-04-18 13:22:10 +0545

    changes

--- Ecommerce Backend API/Program.cs
+++ Ecommerce Backend API/Program.cs
@@ -100,15 +100,23 @@ new UserServiceManager().AddServices(builder.Services);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "EcommerceInstant API v1");
-        c.RoutePrefix = string.Empty;
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "EcommerceInstant API v1");
+    c.RoutePrefix = string.Empty; // Serve at root
+});
+
+
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI(c =>
+//     {
+//         c.SwaggerEndpoint("/swagger/v1/swagger.json", "EcommerceInstant API v1");
+//         c.RoutePrefix = string.Empty;
+//     });
+// }
 
 app.UseHttpsRedirection();
 
